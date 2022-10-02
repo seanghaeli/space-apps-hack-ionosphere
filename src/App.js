ba4dce7e106bf80efd017695fe92017818d63ee1
Sean Ghaeli <seanghaeli@gmail.com>
2022-10-01 21:46:36 -0700

    basic globe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Globe from 'react-globe.gl';
+
 
 function App() {
+  const N = 300;
+  const gData = [...Array(N).keys()].map(() => ({
+    lat: (Math.random() - 0.5) * 180,
+    lng: (Math.random() - 0.5) * 360,
+    size: Math.random() / 3,
+    color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+  }));
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        pointsData={gData}
+        pointAltitude="size"
+        pointColor="color"
+      />
     </div>
   );
 }

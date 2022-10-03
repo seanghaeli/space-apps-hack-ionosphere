d9259bddbd9c0852064fb0c172d5bc1dbfe9be7f
Sean Ghaeli <seanghaeli@gmail.com>
2022-10-02 18:52:13 -0700

    iss data loader script working\

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
 import Globe from 'react-globe.gl';
+import { useEffect, useState } from 'react';
+import jsonData from "./dat.json";
 
 
 function App() {
-  const N = 300;
-  const gData = [...Array(N).keys()].map(() => ({
-    lat: (Math.random() - 0.5) * 180,
-    lng: (Math.random() - 0.5) * 360,
-    size: Math.random() / 3,
-    color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
-  }));
-  
+  const [pointData, setPointData] = useState([]);
+
+  useEffect(() => {
+    const pointData = [...Array(1000).keys()].map((i) => ({
+      lat: jsonData.latitude[i],
+      lng: jsonData.longitude[i],
+      size: 0,
+      color: ['red', 'red', 'red', 'red'][Math.round(1/(1+Math.exp(-(250000000000-jsonData.electronDensity[i])/250000000000)) * 3)]
+    }));
+
+    setPointData(pointData);
+
+    console.log(pointData)
+  }, []);
+
   return (
     <div className="App">
       <Globe
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-        pointsData={gData}
+        pointData={pointData}
         pointAltitude="size"
         pointColor="color"
       />
